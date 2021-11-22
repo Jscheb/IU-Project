641fdfbb6af28c2bf0eaa1e3e749b121bdbf47ea
Nicky <niklas.schubert@smail.inf.h-brs.de>
2021-11-22 17:49:53 +0100

    Timer Prototype

---
+++ Steve/Assets/TimerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TimerController : MonoBehaviour
+{
+    [SerializeField] private TextMesh timer;
+    [SerializeField] private int seconds;
+    [SerializeField] private int minutes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer.text = minutes + ":" + seconds;
+        StartCoroutine("Countdown");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator Countdown()
+    {
+        while (minutes != 0 || seconds != 0)
+        {
+            seconds--;
+            if (seconds < 0)
+            {
+                minutes--;
+                seconds = 59;
+            }
+
+            timer.text = minutes + ":" + seconds;
+            yield return new WaitForSeconds(1);
+        }
+        yield return null;
+    }
+}
